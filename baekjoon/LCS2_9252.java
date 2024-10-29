8707afa5d9014a104904b78f8d85bf6be77d8391
yunaghgh <yunaghgh@naver.com>
2024-10-29 17:09:00 +0900

    refactor: bj 골드4 LCS 2

--- baekjoon/LCS2_9252.java
+++ baekjoon/LCS2_9252.java
@@ -8,42 +8,34 @@ public class LCS2_9252 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] A = br.readLine().trim().toCharArray();
         char[] B = br.readLine().trim().toCharArray();
-        int[][] lcs_list = new int[A.length + 1][B.length + 1];
+        int[][] lcs_arr = new int[A.length + 1][B.length + 1];
 
         for (int a = 1; a <= A.length; a++) {
             for (int b = 1; b <= B.length; b++) {
                 if (A[a - 1] == B[b - 1]) {
-                    lcs_list[a][b] = lcs_list[a - 1][b - 1] + 1;
-                } else if (lcs_list[a][b - 1] < lcs_list[a - 1][b]) {
-                    lcs_list[a][b] = lcs_list[a - 1][b];
+                    lcs_arr[a][b] = lcs_arr[a - 1][b - 1] + 1;
+                } else if (lcs_arr[a][b - 1] < lcs_arr[a - 1][b]) {
+                    lcs_arr[a][b] = lcs_arr[a - 1][b];
                 } else {
-                    lcs_list[a][b] = lcs_list[a][b - 1];
+                    lcs_arr[a][b] = lcs_arr[a][b - 1];
                 }
             }
         }
 
-        System.out.println(lcs_list[A.length][B.length]);
-        if (0 < lcs_list[A.length][B.length]) {
-            int length = lcs_list[A.length][B.length];
-            StringBuilder sb = new StringBuilder();
-            for (int a = A.length; 0 < a; a--) {
-                for (int b = B.length; 0 < b; b--) {
-                    if (lcs_list[a][b] == length) {
-                        while (0 < length) {
-                            if (lcs_list[a][b] == lcs_list[a][b - 1]) {
-                                b--;
-                            } else if (lcs_list[a][b] == lcs_list[a - 1][b]) {
-                                a--;
-                            } else {
-                                sb.append(A[--a]);
-                                length--;
-                            }
-                        }
-                        break;
-                    }
-                }
+        StringBuilder sb = new StringBuilder();
+        int a = A.length;
+        int b = B.length;
+        while (0 < lcs_arr[a][b]) {
+            if (lcs_arr[a][b] == lcs_arr[a][b - 1]) {
+                b--;
+            } else if (lcs_arr[a][b] == lcs_arr[a - 1][b]) {
+                a--;
+            } else {
+                sb.append(A[--a]);
+                b--;
             }
-            System.out.print(sb.reverse());
         }
+        System.out.println(lcs_arr[A.length][B.length]);
+        System.out.print(sb.reverse());
     }
 }
